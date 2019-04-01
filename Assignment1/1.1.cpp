d0cf442583b3e91272c5093819690ddf7d829000
Drish10 <44743273+ohwhatafool@users.noreply.github.com>
2019-04-01 15:22:06 +0200

    1.1.cpp
    inputs your name and prints it

---
+++ Assignment1/1.1.cpp
@@ -0,0 +1,23 @@
+/*
+CH08-320142
+problem 1.1.cpp
+Drishti Maharjan
+d.maharjan@jacobs-university.de
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string first_name;
+
+    getline(cin, first_name); //input of first name
+
+    cout << "My first name is " << first_name << endl; //output of the first name
+
+    return 0;
+
+
+}
